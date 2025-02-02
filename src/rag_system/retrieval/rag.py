345614fa8c82e4f16a35c612071297bddb5df876
Dnau15 <15dnau@gmail.com>
2025-02-03 00:29:51 +0300

    fix: fix bugs related to logging and database creation

--- src/rag_system/retrieval/rag.py
+++ src/rag_system/retrieval/rag.py
@@ -2,35 +2,22 @@ import fire
 import logging
 from pymilvus import MilvusClient
 from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
 from langchain_huggingface import HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from transformers import BertForSequenceClassification, BertTokenizer
 import torch
 import pandas as pd
+from typing import List
+
+from src.rag_system.models.bert_classifier import predict_class
+from src.rag_system.data.data_preprocessing import insert_documents
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pymilvus").setLevel(logging.WARNING)
-
-
-def predict_class(
-    model: BertForSequenceClassification,
-    text: str,
-    tokenizer: BertTokenizer,
-    device: torch.device,
-    max_length: int = 512,
-) -> int:
-    model.eval()
-    inputs = tokenizer(
-        text, return_tensors="pt", max_length=max_length, truncation=True, padding=True
-    )
-    inputs = {k: v.to(device) for k, v in inputs.items()}
-    with torch.no_grad():
-        logits = model(**inputs).logits
-    return torch.argmax(logits, dim=-1).item()
+logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
 
 
 def rag_query(
@@ -77,44 +64,40 @@ def rag_query(
     return llm_chain.invoke(chain_input), predicted_class
 
 
-def start_vector_database(
-    collection_name: str, milvus_uri: str = "./data/milvus_demo.db"
+def load_vector_database(
+    collection_name: str,
+    embedding_model: SentenceTransformer,
+    contexts: List[str],
+    milvus_uri: str = "./data/milvus_demo.db",
 ) -> MilvusClient:
     embedding_dim = 768
     milvus_client = MilvusClient(uri=milvus_uri)
 
     if milvus_client.has_collection(collection_name):
-        milvus_client.drop_collection(collection_name)
+        logging.info("Collection already exists, loading collection")
+        milvus_client.load_collection(collection_name=collection_name)
+    else:
+        logging.info("Creating collection")
 
-    milvus_client.create_collection(
-        collection_name=collection_name,
-        dimension=embedding_dim,
-        metric_type="L2",
-        auto_id=True,
-        primary_field_name="id",
-        vector_field_name="embedding",
-        enable_dynamic_field=True,
-        index_params={
-            "index_type": "IVF_FLAT",
-            "metric_type": "L2",
-            "params": {"nlist": 128},
-        },
-    )
+        milvus_client.create_collection(
+            collection_name=collection_name,
+            dimension=embedding_dim,
+            metric_type="L2",
+            auto_id=True,
+            primary_field_name="id",
+            vector_field_name="embedding",
+            enable_dynamic_field=True,
+            index_params={
+                "index_type": "IVF_FLAT",
+                "metric_type": "L2",
+                "params": {"nlist": 128},
+            },
+        )
+
+        logging.info("Inserting documents")
+        insert_documents(contexts, milvus_client, embedding_model, collection_name)
+        logging.info("Documents inserted")
     return milvus_client
-
-
-def insert_documents(
-    contexts: list[str],
-    milvus_client: MilvusClient,
-    embedding_model: SentenceTransformer,
-    collection_name: str,
-) -> None:
-    documents = [
-        {"text": ctx, "embedding": embedding_model.encode(ctx).tolist()}
-        for ctx in contexts
-    ]
-    for i in tqdm(range(0, len(documents), 100), disable=True):
-        milvus_client.insert(collection_name, documents[i : i + 100])
 
 
 def test_rag_system(
@@ -123,10 +106,9 @@ def test_rag_system(
     bert_path: str = "./models/bert-text-classification-model",
     tokenizer_path: str = "./models/bert-text-classification-model",
     embedding_model_path: str = "sentence-transformers/all-mpnet-base-v2",
+    milvus_uri: str = "./data/milvus_demo.db",
 ):
     load_dotenv()
-
-    milvus_client = start_vector_database(collection_name)
 
     dataset = pd.read_csv("./data/test.csv")
 
@@ -143,7 +125,10 @@ def test_rag_system(
     ).tolist()
 
     embedding_model = SentenceTransformer(embedding_model_path)
-    insert_documents(contexts, milvus_client, embedding_model, collection_name)
+
+    milvus_client = load_vector_database(
+        collection_name, embedding_model, contexts, milvus_uri
+    )
 
     llm = HuggingFaceEndpoint(
         repo_id="mistralai/Mistral-7B-Instruct-v0.2",
