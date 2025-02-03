1362247d31f8bc3b85fa935e2e08fcad0ea550e6
Dnau15 <15dnau@gmail.com>
2025-02-03 22:31:27 +0300

    upd: rewrite rag pipeline to a class

--- src/rag_system/retrieval/rag.py
+++ src/rag_system/retrieval/rag.py
@@ -1,4 +1,3 @@
-import fire
 import logging
 from pymilvus import MilvusClient
 from sentence_transformers import SentenceTransformer
@@ -8,10 +7,16 @@ from langchain_core.prompts import PromptTemplate
 from transformers import BertForSequenceClassification, BertTokenizer
 import torch
 import pandas as pd
-from typing import List
+from typing import List, Optional
+from pathlib import Path
+import os
+import time
 
 from src.rag_system.models.bert_classifier import predict_class
 from src.rag_system.data.data_preprocessing import insert_documents
+
+# Get project root (where this script resides)
+PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -20,157 +25,166 @@ logging.getLogger("pymilvus").setLevel(logging.WARNING)
 logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
 
 
-def rag_query(
-    llm: HuggingFaceEndpoint,
-    question: str,
-    classificator: BertForSequenceClassification,
-    tokenizer: BertTokenizer,
-    milvus_client: MilvusClient,
-    device: torch.device,
-    embedding_model: SentenceTransformer,
-    collection_name: str,
-    use_classifier: bool = True,
-    top_k: int = 3,
-    context_len: int = 400,
-) -> tuple[str, int]:
-    query_embedding = embedding_model.encode(question).tolist()
+class RAGPipeline:
+    def __init__(
+        self,
+        collection_name: str = "spbrag",
+        bert_path: Optional[str] = None,
+        tokenizer_path: str = "bert-base-uncased",
+        embedding_model_path: str = "sentence-transformers/all-mpnet-base-v2",
+        milvus_uri: Optional[str] = None,
+        llm_repo_id: str = "mistralai/Mistral-7B-Instruct-v0.2",
+        llm_max_new_tokens: int = 100,
+        llm_do_sample: bool = False,
+    ):
+        bert_path = bert_path or os.path.join(
+            PROJECT_ROOT, "./models/bert-text-classification-model"
+        )
+        milvus_uri = milvus_uri or os.path.join(PROJECT_ROOT, "./data/milvus_demo.db")
 
-    if use_classifier:
-        predicted_class = predict_class(classificator, question, tokenizer, device)
-    else:
-        predicted_class = 1
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.embedding_model = SentenceTransformer(embedding_model_path)
+        self.classificator = BertForSequenceClassification.from_pretrained(
+            bert_path, num_labels=2
+        )
+        self.classificator.to(self.device)
+        self.tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
+        self.llm = HuggingFaceEndpoint(
+            repo_id=llm_repo_id,
+            task="text-generation",
+            max_new_tokens=llm_max_new_tokens,
+            do_sample=llm_do_sample,
+        )
+        self.collection_name = collection_name
+        self.milvus_uri = milvus_uri
+        self.milvus_client = None
 
-    search_results = milvus_client.search(
-        collection_name, [query_embedding], limit=top_k, output_fields=["text"]
-    )
-    contexts = (
-        [hit["entity"]["text"] for hit in search_results[0]] if search_results else []
-    )
-    context_str = " ".join(contexts)[:context_len]
+    def load_vector_database(self, contexts: List[str]) -> None:
+        self.milvus_client = MilvusClient(uri=self.milvus_uri)
+        if self.milvus_client.has_collection(self.collection_name):
+            self.milvus_client.load_collection(collection_name=self.collection_name)
+        else:
+            embedding_dim = self.embedding_model.get_sentence_embedding_dimension()
+            self.milvus_client.create_collection(
+                collection_name=self.collection_name,
+                dimension=embedding_dim,
+                metric_type="L2",
+                auto_id=True,
+                primary_field_name="id",
+                vector_field_name="embedding",
+                enable_dynamic_field=True,
+                index_params={
+                    "index_type": "IVF_FLAT",
+                    "metric_type": "L2",
+                    "params": {"nlist": 128},
+                },
+            )
+            insert_documents(
+                contexts, self.milvus_client, self.embedding_model, self.collection_name
+            )
 
-    template = (
-        "Answer the question based on context:\nContext: {context}\nQuestion: {question}\nAnswer:"
-        if contexts and (use_classifier and predicted_class)
-        else "Answer this question:\nQuestion: {question}\nAnswer:"
-    )
+    def query_with_classifier(
+        self,
+        question: str,
+        top_k: int = 3,
+        context_len: int = 400,
+    ) -> tuple[str, int, float]:
+        # TODO add time
 
-    prompt_template = PromptTemplate.from_template(template)
-    llm_chain = prompt_template | llm
-    chain_input = (
-        {"question": question, "context": context_str}
-        if contexts
-        else {"question": question}
-    )
-    return llm_chain.invoke(chain_input), predicted_class
+        start_time = time.time()
+        predicted_class = predict_class(
+            self.classificator, question, self.tokenizer, self.device
+        )
 
+        contexts = []
+        if predict_class:
+            query_embedding = self.embedding_model.encode(question).tolist()
+            search_results = self.milvus_client.search(
+                self.collection_name,
+                [query_embedding],
+                limit=top_k,
+                output_fields=["text"],
+            )
 
-def load_vector_database(
-    collection_name: str,
-    embedding_model: SentenceTransformer,
-    contexts: List[str],
-    milvus_uri: str = "./data/milvus_demo.db",
-) -> MilvusClient:
-    embedding_dim = 768
-    milvus_client = MilvusClient(uri=milvus_uri)
+            contexts = (
+                [hit["entity"]["text"] for hit in search_results[0]]
+                if search_results
+                else []
+            )
 
-    if milvus_client.has_collection(collection_name):
-        logging.info("Collection already exists, loading collection")
-        milvus_client.load_collection(collection_name=collection_name)
-    else:
-        logging.info("Creating collection")
+            context_str = " ".join(contexts)[:context_len]
 
-        milvus_client.create_collection(
-            collection_name=collection_name,
-            dimension=embedding_dim,
-            metric_type="L2",
-            auto_id=True,
-            primary_field_name="id",
-            vector_field_name="embedding",
-            enable_dynamic_field=True,
-            index_params={
-                "index_type": "IVF_FLAT",
-                "metric_type": "L2",
-                "params": {"nlist": 128},
+        template = (
+            "Answer the question based on context:\nContext: {context}\nQuestion: {question}\nAnswer:"
+            if contexts
+            else "Answer this question:\nQuestion: {question}\nAnswer:"
+        )
+
+        llm_chain = PromptTemplate.from_template(template) | self.llm
+
+        result = llm_chain.invoke(
+            {"question": question, "context": context_str}
+            if contexts
+            else {"question": question}
+        )
+        execution_time = round(time.time() - start_time, 2)
+        return result, predicted_class, execution_time
+
+    def query_without_classifier(
+        self, question: str, top_k: int = 3, context_len: int = 400
+    ) -> tuple[str, int, float]:
+        start_time = time.time()
+
+        query_embedding = self.embedding_model.encode(question).tolist()
+        predicted_class = predict_class(
+            self.classificator, question, self.tokenizer, self.device
+        )
+
+        search_results = self.milvus_client.search(
+            self.collection_name, [query_embedding], limit=top_k, output_fields=["text"]
+        )
+        contexts = (
+            [hit["entity"]["text"] for hit in search_results[0]]
+            if search_results
+            else []
+        )
+        context_str = " ".join(contexts)[:context_len]
+        template = (
+            "Answer the question based on context:\nContext: {context}\nQuestion: {question}\nAnswer:"
+            if contexts
+            else "Answer this question:\nQuestion: {question}\nAnswer:"
+        )
+        llm_chain = PromptTemplate.from_template(template) | self.llm
+
+        result = llm_chain.invoke(
+            {"question": question, "context": context_str}
+            if contexts
+            else {"question": question}
+        )
+        execution_time = round(time.time() - start_time, 2)
+        return result, predicted_class, execution_time
+
+    def test(
+        self,
+        dataset_path: str = "./data/test.csv",
+        num_samples: int = 10,
+    ) -> None:
+        load_dotenv()
+        dataset = pd.read_csv(dataset_path)
+        contexts = dataset["context"].dropna().unique().tolist()
+        self.load_vector_database(contexts)
+        qa_pairs = dataset.apply(
+            lambda row: {
+                "question": row["question"],
+                "answer": row["answers"],
+                "need_retrieval": row["need_retrieval"],
             },
-        )
-
-        logging.info("Inserting documents")
-        insert_documents(contexts, milvus_client, embedding_model, collection_name)
-        logging.info("Documents inserted")
-    return milvus_client
-
-
-def test_rag_system(
-    num_samples: int = 10,
-    collection_name: str = "spbrag",
-    bert_path: str = "./models/bert-text-classification-model",
-    tokenizer_path: str = "./models/bert-text-classification-model",
-    embedding_model_path: str = "sentence-transformers/all-mpnet-base-v2",
-    milvus_uri: str = "./data/milvus_demo.db",
-):
-    load_dotenv()
-
-    dataset = pd.read_csv("./data/test.csv")
-
-    contexts = dataset["context"].dropna().unique().tolist()
-
-    qa_pairs = dataset.apply(
-        lambda row: {
-            "question": row["question"],
-            "answer": row["answers"],
-            "context": row["context"],
-            "need_retrieval": row["need_retrieval"],
-        },
-        axis=1,
-    ).tolist()
-
-    embedding_model = SentenceTransformer(embedding_model_path)
-
-    milvus_client = load_vector_database(
-        collection_name, embedding_model, contexts, milvus_uri
-    )
-
-    llm = HuggingFaceEndpoint(
-        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
-        task="text-generation",
-        max_new_tokens=100,
-        do_sample=False,
-    )
-
-    num_labels = 2
-    classificator = BertForSequenceClassification.from_pretrained(
-        bert_path, num_labels=num_labels
-    )
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    classificator.to(device)
-    tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
-
-    logging.info("\nTesting RAG system...\n")
-    logging.info("-" * 160)
-
-    for qa in qa_pairs[:num_samples]:
-        query = qa["question"]
-        result, predicted_class = rag_query(
-            llm,
-            query,
-            classificator,
-            tokenizer,
-            milvus_client,
-            device,
-            embedding_model,
-            collection_name,
-        )
-        generated_answer = result
-        true_answer = qa["answer"]
-        need_retrieval = qa["need_retrieval"]
-
-        logging.info(
-            f"Question: {query[:45]} | Predicted class: {predicted_class} | True class: {need_retrieval}"
-        )
-        logging.info(f"Generated answer: {generated_answer[:100]}")
-        logging.info(f"True answer: {true_answer}")
-
-
-if __name__ == "__main__":
-    fire.Fire(test_rag_system)
+            axis=1,
+        ).tolist()
+        for qa in qa_pairs[:num_samples]:
+            result, predicted_class = self.query(qa["question"])
+            logging.info(
+                f"Question: {qa['question'][:45]} | Predicted: {predicted_class} | True: {qa['need_retrieval']}"
+            )
+            logging.info(f"Generated: {result[:100]}")
+            logging.info(f"True answer: {qa['answer']}")
